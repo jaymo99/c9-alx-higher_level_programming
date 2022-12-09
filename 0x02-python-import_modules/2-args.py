31e36aead94ba736d3614d7fdabd23f7fa82e8f4
James Kariuki <karisjaymo99@gmail.com>
2022-12-09 12:57:20 +0300

    remove extra space when printing no of args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ import sys
 
 n = len(sys.argv)
 
-print(f"{n - 1} arguments: " if (n - 1) != 1 else f"{n - 1} argument: ")
+print(f"{n - 1} arguments:" if (n - 1) != 1 else f"{n - 1} argument:")
 
 for i in range(1, n):
     print(f"{i}: {sys.argv[i]}")
