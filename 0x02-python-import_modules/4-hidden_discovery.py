00b902cff8b321cb1e51359a2ed2bb48c1c1a68e
James Kariuki <karisjaymo99@gmail.com>
2022-12-10 23:58:54 -0800

    Task 4 - print all defined names in a module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,7 @@ if __name__ != "__main__":
 
 import hidden_4
 
-for i in dir(hidden_4):
-    if not (i[0] == '_' and i[1] == '_'):
+names = dir(hidden_4)
+for i in names:
+    if (i[0] != '_' and i[1] != '_'):
         print(i)
