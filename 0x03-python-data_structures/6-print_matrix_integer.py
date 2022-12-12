5f5ce4db04a3becf3996279187144fa398a415a9
James Kariuki <karisjaymo99@gmail.com>
2022-12-12 12:36:29 +0300

    Task 6 - print a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        print()
+
+    for row in matrix:
+        for element in row:
+            print("{:d}", element, end='')
+            if element is not row[len(row) - 1]:
+                print(" ", end='')
+            else:
+                print("")
