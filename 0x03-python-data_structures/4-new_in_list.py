cf97e7890e71bd8a55af80e192ce7254ae5a5f33
James Kariuki <karisjaymo99@gmail.com>
2022-12-12 11:43:31 +0300

    copy list before any checks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if my_list is None:
-        return
-    if idx >= len(my_list):
-        return
-
     new_list = my_list.copy()
+
+    if my_list is None:
+        return new_list
+    if idx < 0:
+        return new_list
+    if idx >= len(my_list):
+        return new_list
+
     new_list[idx] = element
     return new_list
