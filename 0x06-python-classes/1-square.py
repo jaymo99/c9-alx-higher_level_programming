77bf4038b4a056b51e0cd05a05c853d04b81b054
James Kariuki <karisjaymo99@gmail.com>
2023-01-03 17:56:41 +0300

    Task 1 - square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class, Square"""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize square.
+
+        Args:
+            size (int): Size of the square
+        """
+    self.__size = size
