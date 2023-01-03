1846ad17adcec4dc758bb81e93537bab9e6d1030
James Kariuki <karisjaymo99@gmail.com>
2023-01-03 18:01:49 +0300

    preceding blank line in module docstring, Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 """Defines a class, Square."""
 
 
 class Square:
     """Defines a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         """Initialize square.
 
         Args:
