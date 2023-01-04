1fe4c9e44f59aa8a735c358e8aee57dcfd3cae47
James Kariuki <karisjaymo99@gmail.com>
2023-01-04 19:17:17 +0300

    Test init method with setters and getters

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,8 +12,8 @@ class Square:
             size (int): size of the square object
             position (int, int): the position of the square object
         '''
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
