92a327ad5ef0db33a1f1e3d104d5014592207743
James Kariuki <karisjaymo99@gmail.com>
2023-01-07 21:04:48 +0300

    Task 0 - integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Defines a function that adds to integers
+'''
+
+def add_integer(a, b=98):
+    ''' Returns the sum of 2 integers.
+
+    Args:
+        a: First integer
+        b: Second integer
+    '''
+    if (type(a) is not int) and (type(a) is not float):
+        raise TypeError("a must be an integer");
+    if (type(b) is not int) and (type(b) is not float):
+        raise TypeError("b must be an integer");
+    a = int(a)
+    b = int(b)
+
+    return (a + b)
