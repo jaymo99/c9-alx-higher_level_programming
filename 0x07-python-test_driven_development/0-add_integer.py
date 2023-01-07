829aaa5ffd5a64567d625bcb321183ee6724a9c8
James Kariuki <karisjaymo99@gmail.com>
2023-01-07 21:21:27 +0300

    pycodestyle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 Defines a function that adds to integers
 '''
 
+
 def add_integer(a, b=98):
     ''' Returns the sum of 2 integers.
 
@@ -11,9 +12,9 @@ def add_integer(a, b=98):
         b: Second integer
     '''
     if (type(a) is not int) and (type(a) is not float):
-        raise TypeError("a must be an integer");
+        raise TypeError("a must be an integer")
     if (type(b) is not int) and (type(b) is not float):
-        raise TypeError("b must be an integer");
+        raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
 
