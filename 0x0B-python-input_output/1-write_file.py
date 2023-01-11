2d6389c22275f02f185bd4456428e495c83d70ac
James Kariuki <karisjaymo99@gmail.com>
2023-01-11 17:29:13 +0300

    add function docstring for write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,13 @@
 
 
 def write_file(filename="", text=""):
+    '''writes a string to a text file (UTF8).
+
+    Args:
+        filename (str): Name of the file to write to
+        text (str): text string to write into the file
+    Returns:
+        The number of characters written.'''
+
     with open(filename, 'w', encoding='utf-8') as wr_file:
         return wr_file.write(text)
