6a9f93bb3edae385f4af7d0f3518fbc86faf20bf
James Kariuki <karisjaymo99@gmail.com>
2023-01-15 13:09:50 +0300

    add docstring for say_my_name()

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,11 +6,19 @@ Module defines one function:
 
 
 def say_my_name(first_name, last_name=""):
+    '''Prints a message with the name of the person.
+
+    Args:
+        first_name (str): The first name of the person.
+        last_name (str, optional): The last name of the person.
+            Defaults to "".
+
+    Raises:
+        TypeError: If firs_name or last_name is not a string.
+    '''
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    if last_name:
-        print("My name is {} {}".format(first_name, last_name))
-    else:
-        print("My name is {}".format(first_name))
+    print("My name is {} {}".format(first_name, last_name))
