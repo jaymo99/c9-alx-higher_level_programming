7ab6cac8c1bf76526bbfc5e1a398c4434abd763a
James Kariuki <karisjaymo99@gmail.com>
2023-01-20 07:53:02 +0300

    unittest for Base class

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''unittest for Base()
+'''
+
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    def test_init(self):
+        obj1 = Base()
+        self.assertEqual(obj1.id, 1)
+
+        obj2 = Base()
+        self.assertEqual(obj2.id, 2)
+
+        obj3 = Base(12)
+        self.assertEqual(obj3.id, 12)
